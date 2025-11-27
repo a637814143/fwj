f941eaca7c086abf73c864785a7deb8fbc6f2fab
a637814143 <140132912+a637814143@users.noreply.github.com>
2025-11-27 19:40:27 +0800

    Enable MailProperties binding for mail config

--- demo/src/main/java/com/example/demo/config/MailConfig.java
+++ demo/src/main/java/com/example/demo/config/MailConfig.java
@@ -3,12 +3,14 @@ package com.example.demo.config;
 import java.util.Properties;
 
 import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
+@EnableConfigurationProperties(MailProperties.class)
 public class MailConfig {
 
     @Bean

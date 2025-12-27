3fdef86802878bcbce6240fb1e09757a52efcaf8
a637814143 <140132912+a637814143@users.noreply.github.com>
2025-12-27 12:52:15 +0800

    Add missing validation annotation import

--- demo/src/main/java/com/example/demo/house/SecondHandHouseRequest.java
+++ demo/src/main/java/com/example/demo/house/SecondHandHouseRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
